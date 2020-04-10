0e752d4ac04bf57dc5f3a163536cc26ad5bd5491
Shuchangd <52063883+Shuchangd@users.noreply.github.com>
2020-04-10 13:22:26 -0400

    lab1 finished
    task 7 done

--- lab/lab1/js/draw.js
+++ lab/lab1/js/draw.js
@@ -7,9 +7,9 @@ Try to use one or two of the drawing tools. They should allow you to draw
 without needing any additional configuration. These shapes will not be added to
 the map. We'll fix that in the next task.*/
 
+// Check out the turf function
 var polygon = turf.circle([40.000, -85.1639], 2, {steps: 10, units: 'kilometers'});
 var point = turf.point([-75.3221, 39.529]);
-// "a", "b", and "c" values represent the values of the coordinates in order.
 var triangle = turf.polygon([[
   [-75.1221, 39.57],
   [-75.58, 39.18],
@@ -62,14 +62,14 @@ If you get the error: "Cannot read property '_leaflet_id' of undefined", it
 may be because you are trying to remove a layer that does not yet exist.
 Check to see if myRectangle is defined before trying to remove it. */
 
-var layer = [0,0];
+/* var layer = [0,0];
 map.on(L.Draw.Event.CREATED, function(e){
     map.removeLayer(layer)
     var newlayer = e.layer;
     layer = e.layer;
-    map.addLayer(newlayer)})
+    map.addLayer(newlayer)}) */
 
-
+//myrectangle.push(layer)
 /* Task 4: Add shape to sidebar
 
 Let's add the shape we've created to the sidebar. In the HTML, there is a
@@ -102,9 +102,24 @@ Where [the id] is replaced by the Leaflet ID of the layer.
 When a new layer is added, you can use jQuery's empty function to clear
 out the #shapes container before appending a new .shape.
 
+jhtml = $.parseHTML('<div ')
+$('#shapes').append(jhtml)*/
+
+/* var layer = [0,0];
+var obj
+map.on(L.Draw.Event.CREATED, function(e){
+    map.removeLayer(layer)
+    var newlayer = e.layer;
+    obj = e;
+    layer = e.layer;
+    var id = L.stamp(layer);
+    var jhtml=$.parseHTML(`<div data-leaflet-id=${id}><h1>Current ID: ${id}</h1></div>`);
+    $('#shapes').empty();
+    $('#shapes').append(jhtml);
+    map.addLayer(newlayer)}) */
 
 
-Task 5: Store multiple shapes
+/* Task 5: Store multiple shapes
 
 Instead of showing one shape at a time, let's allow multiple shapes to be drawn.
 Instead of storing one Leaflet layer in the myRectangle variable, we should use
@@ -112,9 +127,26 @@ an array to store multiple layers. There will also be multiple shapes displayed
 in the sidebar.
 
 Change the variable myRectangle to myRectangles and set it to equal an empty
-array. Change the rest of your code to push new layers into the array.
+array. Change the rest of your code to push new layers into the array.*/
 
-Task 6: Connect sidebar and map
+
+/* 
+var layers = [];
+
+map.on(L.Draw.Event.CREATED, function(e){
+    var newlayer = e.layer;
+    layers.push({"shape": newlayer});
+    var id = L.stamp(newlayer);
+    var jhtml=$.parseHTML(`<div data-leaflet-id=${id}><h1>Current ID: ${id}</h1></div>`);
+    $('#shapes').empty();
+    $('#shapes').append(jhtml);
+    map.addLayer(newlayer)}) 
+
+*/
+
+
+
+/* Task 6: Connect sidebar and map
 
 The HTML in the sidebar and the Leaflet layers on the map and in our Javascript
 variable can be linked by using the Leaflet ID. Modify the application so that
@@ -123,9 +155,14 @@ clicking on a shape in the sidebar will do one of the following:
 - Change the color of the corresponding shape on the map
 - Delete the corresponding shape on the map (be sure to remove it from the
 sidebar and the myRectanges array)
-- Anything else you can think of!
+- Anything else you can think of! */
 
-Task 7 (Stretch Goal): Reverse Task 6
+// This section will be combined with section 7
+
+
+
+
+/* Task 7 (Stretch Goal): Reverse Task 6
 
 Modify the application so moving your mouse over a rectangle on the map will
 highlight (change style in some way) the corresponding element in the sidebar.
@@ -156,3 +193,35 @@ map.on('draw:created', function (e) {
     var layer = e.layer; // The Leaflet layer for the shape
     var id = L.stamp(layer); // The unique Leaflet ID for the layer
 });
+
+
+var layers = [];
+
+map.on(L.Draw.Event.CREATED, function(e){
+    var newlayer = e.layer;
+    var id = L.stamp(newlayer);
+    layers.push({"shape": newlayer,
+                 "id":id});
+    var jhtml=$.parseHTML(`<div data-leaflet-id=${id}><h1>Current ID: ${id}</h1></div>`);
+    $('#shapes').append(jhtml);
+    _.map(layers, function(arr){
+        map.addLayer(arr.shape);
+        arr.shape.on("mouseover", function(e) {
+            $(`[data-leaflet-id=${e.target._leaflet_id}]`).css("background-color", "red");
+        });
+        arr.shape.on("mouseout", function(e) {
+            $(`[data-leaflet-id=${e.target._leaflet_id}]`).css("background-color", "transparent");
+        });
+    })
+    $(`[data-leaflet-id=${id}]`).click(function(e) {
+         _.map(layers, function(arr){
+            if(arr.id='${id}'){
+               map.removeLayer(arr.shape);
+            }
+        })
+        $(`[data-leaflet-id=${layer['id']}]`).remove();
+      });
+
+}) 
+
+
